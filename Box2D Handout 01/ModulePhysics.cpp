96601aae01748f5922d45f23fa47b317485d3542
Linjiajie2002 <92087408+Linjiajie2002@users.noreply.github.com>
2023-10-16 09:33:38 +0200

    JJL-UPDATE

--- Box2D Handout 01/ModulePhysics.cpp
+++ Box2D Handout 01/ModulePhysics.cpp
@@ -15,11 +15,19 @@
 
 #define GRAVITY_X 0.0f
 #define GRAVITY_Y -7.0f
+#define PIXELS_PER_METER 50.0f // if touched change METER_PER_PIXEL too
+#define METER_PER_PIXEL 0.02f // this is 1 / PIXELS_PER_METER !
+
+#define METERS_TO_PIXELS(m) ((int) floor(PIXELS_PER_METER * m))
+#define PIXEL_TO_METERS(p)  ((float) METER_PER_PIXEL * p)
+
 
 
 class b2World;
 class b2Body;
+class b2BodyDef;
 b2World* world;
+
 
 ModulePhysics::ModulePhysics(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
@@ -51,6 +59,25 @@ bool ModulePhysics::Start()
 
 
 	// TODO 4: Create a a big static circle as "ground"
+	// big static circle as "ground" in the middle of the screen
+	int x = SCREEN_WIDTH / 2;
+	int y = SCREEN_HEIGHT / 1.5f;
+	int diameter = SCREEN_WIDTH / 2;
+
+	b2BodyDef body;
+	body.type = b2_staticBody;
+	body.position.Set(PIXEL_TO_METERS(x), PIXEL_TO_METERS(y));
+
+	b2Body* b = world->CreateBody(&body);
+
+	b2CircleShape shape;
+	shape.m_radius = PIXEL_TO_METERS(diameter) * 0.5f;
+
+	b2FixtureDef fixture;
+	fixture.shape = &shape;
+	b->CreateFixture(&fixture);
+
+
 	return true;
 }
 
@@ -58,6 +85,8 @@ bool ModulePhysics::Start()
 update_status ModulePhysics::PreUpdate()
 {
 	// TODO 3: Update the simulation ("step" the world)
+	world->Step(1.0f / 60.0f, 6, 2);
+
 
 	return UPDATE_CONTINUE;
 }
